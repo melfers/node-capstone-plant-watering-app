6d6b78594c88664a15c57961ca6e281ac3ebe382
Molly Elfers <elfers.molly@gmail.com>
2019-01-04 19:30:49 -0800

    added server test

---
+++ test/test-server.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+// Import server.js and use destructuring assignment to create variables for
+// server.app, server.runServer, and server.closeServer
+const {app, runServer, closeServer} = require('../server');
+
+// declare a variable for expect from chai import
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Static', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+  // `chai.request.get` is an asynchronous operation. When
+  // using Mocha with async operations, we need to either
+  // return an ES6 promise or else pass a `done` callback to the
+  // test that we call at the end. We prefer the first approach, so
+  // we just return the chained `chai.request.get` object.
+  it('should return a static html file', function() {
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+      });
+  });
+});
